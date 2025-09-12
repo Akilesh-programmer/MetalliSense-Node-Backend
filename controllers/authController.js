c46f274cb28e363b48d414b241d8bc08d8bbcd23
Akilesh <akileshakileshs1234@gmail.com>
2025-09-13 01:03:22 +0530

    updated samsSite  policy

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,7 +18,7 @@ const createSendToken = (user, statusCode, res) => {
     ),
     httpOnly: true,
     secure: true, // Only send over HTTPS in production
-    sameSite: 'true', // Helps with CSRF protection
+    sameSite: 'none', // Helps with CSRF protection
   };
 
   res.cookie('jwt', token, cookieOptions);
